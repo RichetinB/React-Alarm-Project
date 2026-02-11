eceed53201380765c4f595b3f5d131db3b3314ec
RichetinB <baptisterichetin@gmail.com>
2026-02-11 15:08:53 +0100

    :construction: ajout de swagger

---
+++ backend-nest/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const databaseUrl = process.env.DATABASE_URL;
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not set');
+    }
+
+    const pool = new Pool({
+      connectionString: databaseUrl,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BuzzBrain API')
+    .setDescription('API documentation for BuzzBrain application')
+    .setVersion('1.0')
+    .addTag('buzzbrain')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Application is running on: localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
